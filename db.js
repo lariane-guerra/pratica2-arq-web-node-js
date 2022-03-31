a005c2a2adf4e39b92c14c4758309ad0ff88da19
lariane-guerra <sguerra.lariane@gmail.com>
2022-03-31 08:40:43 -0300

    firstcommit

---
+++ db.js
@@ -0,0 +1,25 @@
+const mongoClient = require("mongodb").MongoClient;
+mongoClient
+  .connect("mongodb://localhost")
+  .then((conn) => (global.conn = conn.db("apimovies")))
+  .catch((err) => console.log(err));
+
+function findAll() {
+  return global.conn.collection("movies").find().toArray();
+}
+
+function insert(movie) {
+  return global.conn.collection("movies").insertOne(movie);
+}
+const ObjectId = require("mongodb").ObjectId;
+
+function findOne(id) {
+  return global.conn.collection("movies").findOne(new ObjectId(id));
+}
+function update(id, movie) {
+  return global.conn.collection("movies").updateOne({ _id: new ObjectId(id) }, { $set: movie });
+}
+function deleteOne(id) {
+  return global.conn.collection("movies").deleteOne({ _id: new ObjectId(id) });
+}
+module.exports = { findAll , insert, findOne, update, deleteOne  };
